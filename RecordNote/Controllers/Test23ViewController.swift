ea9967cc4ddcc7211fa9d0c90c2c783dfdd33d12
amaocha <guiltaugusthird03@gmail.com>
2018-12-19 04:11:32 +0900

    add test23.

---
+++ RecordNote/Controllers/Test23ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Test23ViewController.swift
+//  RecordNote
+//
+//  Created by coco j on 2018/12/19.
+//  Copyright © 2018 coco j. All rights reserved.
+//
+
+import UIKit
+
+class Test23ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
